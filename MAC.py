5580966dd7dca6524998828d35c94267551b38df
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-03-24 19:15:17 +0530

    Update MAC.py

--- MAC.py
+++ MAC.py
@@ -5,7 +5,7 @@ class MAC:
         self.goal=((0,0,0),(n,n,1))
 
     def bfs(self):
-        print("BFS: \n")
+        print('\nBFS : ')
         opened,closed=[],[]
         opened.append(((self.n,self.n,1),(0,0,0)))
         print("state-x","open","close",sep='\t\t\t')
@@ -14,10 +14,10 @@ class MAC:
             p=opened.pop(0)
             closed.append(p)
             (lm,lc,lb),(rm,rc,rb)=p
-            print(lm,lc,lb,rm,rc,rb)
             # Goal case
             if p==self.goal:
-                print(p,opened,closed);print("Success");print("Completed in ",count," steps");return
+                print('p = ',p);print('Open = ',opened);print('Close = ',closed)
+                print("Success");print("Completed in ",count," steps");return
             # if boat is on left
             if lb==1:
                 # Rule - 1 shift 2 missionaries
@@ -62,11 +62,11 @@ class MAC:
                 if rm-1>=0 and (rm-1>=rc or rm-1==0) and lm+1<=3 and lm+1>=lc:
                     temp=((lm+1,lc,1),(rm-1,rc,0))
                     if temp not in opened and temp not in closed:opened.append(temp)
-            print(p,opened,closed,sep='\t\t\t');count+=1
+            print('p = ',p);print('Open = ',opened);print('Close = ',closed);count+=1
         print("Goal not found")
 
     def dfs(self):
-        print("DFS: \n")
+        print('\nDFS : ')
         opened,closed=[],[]
         opened.append(((self.n,self.n,1),(0,0,0)))
         print("state-x","open","close",sep='\t\t\t')
@@ -75,10 +75,10 @@ class MAC:
             p=opened.pop()
             closed.append(p)
             (lm,lc,lb),(rm,rc,rb)=p
-            print(lm,lc,lb,rm,rc,rb)
             # Goal case
             if p==self.goal:
-                print(p,opened,closed);print("Success");print("Completed in ",count," steps");return
+                print('p = ',p);print('Open = ',opened);print('Close = ',closed)
+                print("Success");print("Completed in ",count," steps");return
             # if boat is on left
             if lb==1:
                 # Rule - 1 shift 2 missionaries
@@ -123,7 +123,7 @@ class MAC:
                 if rm-1>=0 and (rm-1>=rc or rm-1==0) and lm+1<=3 and lm+1>=lc:
                     temp=((lm+1,lc,1),(rm-1,rc,0))
                     if temp not in opened and temp not in closed:opened.append(temp)
-            print(p,opened,closed,sep='\t\t\t');count+=1
+            print('p = ',p);print('Open = ',opened);print('Close = ',closed);count+=1
         print("Goal not found")
 
 
