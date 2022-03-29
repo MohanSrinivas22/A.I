ec24ed8708a238b8af681e7e7b30afc58da1410b
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-03-29 07:40:53 +0530

    Update informedSearch.py

--- informedSearch.py
+++ informedSearch.py
@@ -77,6 +77,59 @@ Goal node found
 
 
 
+''' Branch and bound '''
+from collections import defaultdict
+class Graph:
+  def __init__(self,n):
+    self.n=n
+    self.g={}
+    self.graph=defaultdict(list)
+    
+  def addEdge(self,u,v,i):
+    self.graph[u].append(v)
+    self.graph[v].append(u)
+    if u not in self.g: self.g[u]=0
+    self.g[v]=i+self.g[u]
+  
+  def branch_bounds(self,start,goal):
+    print('Branch and bound Search : ')
+    print("open","close",sep='\t\t\t')
+    opened,closed=[],[]
+    opened.append(start)
+    print(opened, closed, sep='\t\t\t')
+    while opened:
+      p=opened.pop(0)
+      closed.append(p)
+      #Goal node
+      if p==goal:
+        print(opened,closed,sep='\t\t\t')
+        print('Goal node found');return
+      #Successors Generation
+      for v in self.graph[p]:
+        if v not in opened and v not in closed:opened.append(v)
+      opened.sort(key=lambda x:self.g[x])
+      print(opened,closed,sep='\t\t\t')
+    print('Goal node not found')
+    
+n=int(input('Enter no.of nodes: '))
+gr=Graph(n)
+m=int(input('Enter no.of edges: '))
+for _ in range(m):
+  u,v,i=input('Enter edge nodes and heuristic :  ').split()
+  gr.addEdge(u,v,int(i))
+start,goal=input('Enter start and goal states: ').split()
+gr.branch_bounds(start,goal)
+
+# Sample input :-
+
+#Sample output :-
+'''
+
+'''
+
+
+
+
 '''Hill Climbing'''
 from collections import defaultdict
 class Graph:
