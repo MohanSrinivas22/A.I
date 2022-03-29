9378c0815d1560d10d662b6039f1eecded20e67c
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-03-29 13:57:23 +0530

    Update uninformedSearch.py

--- uninformedSearch.py
+++ uninformedSearch.py
@@ -40,6 +40,9 @@ dfs_path=g.dfs(start,goal)
 print(bfs_path)
 print(dfs_path)
 
+
+
+
 '''DFID'''
 from collections import defaultdict
 class Graph:
@@ -75,3 +78,47 @@ for _ in range(n):                              # Enter  Space seperated edge no
     g.addEdge(u,v)
 start,goal=input('Enter start and goal nodes : ').split()
 g.dfid(start,goal)
+
+
+
+
+''' Bidirectional '''
+from collections import defaultdict
+class Graph:
+    def __init__(self):
+        self.graph=defaultdict(list)
+        
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+        
+    def bds(self, start, goal):
+        fopen,bopen,fclose,bclose=[],[],[],[]
+        fopen.append(start)
+        bopen.append(goal)
+        print(fopen,bopen,fclose,bclose,sep='\t\t')
+        while fopen and bopen:
+            p=fopen.pop(0)
+            q=bopen.pop(0)
+            if p==q:print('Goal node found');return
+            if p not in fclose:fclose.append(p)
+            if q not in bclose:bclose.insert(0,q)
+            # Generate Succesors :-
+            for v in self.graph[p]:
+                if v not in fclose:fopen.append(v)
+            for v in self.graph[q]:
+                if v not in bclose:bopen.append(v)
+            print(fopen,bopen,fclose,bclose,sep='\t\t')
+            # Goal Case :-
+            for i in fopen:
+                if i in bopen:print('Goal node found');return
+        print('Goal node not found')
+        
+n=int(input('Enter no.of nodes : '))
+m=int(input('Enter no.of edges : '))
+g=Graph()
+for _ in range(m):
+    u,v=input('Enter edge nodes : ').split()
+    g.addEdge(u,v)
+start, goal=input('Enter start and goal nodes : ').split()
+g.bds(start, goal)
