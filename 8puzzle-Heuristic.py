929f46724f5f72aecacfc4c89ca6285ac8d1b91a
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-04-19 05:50:10 +0530

    Update 8puzzle-Heuristic.py

--- 8puzzle-Heuristic.py
+++ 8puzzle-Heuristic.py
@@ -1,3 +1,87 @@
+# AStar :
+class Graph:
+  def __init__(self):
+      self.g={}
+
+  def gv(self,node,parent):
+    if parent==None:self.g[node]=0;return 0
+    if node in self.g:self.g[node]=min(self.g[node],self.g[parent]+1)
+    else:self.g[node]=self.g[parent]+1
+    return self.g[node]
+
+  def h(self,node):
+    count=0
+    for i in range(9):
+      if node[i]!=self.goal[i]:count+=1
+    return count
+
+  def inopen(self,opened,node,parent,g,h):
+    for i in opened:
+      i=list(i)
+      if i[0]==node:
+        if i[1]>g+h:
+          i[1]=g+h;self.g[i[0]]=self.gv(i[0],parent)
+        elif i[1]==g+h and self.g[i[0]]>g:
+          self.g[i[0]]=self.gv(i[0],parent);i[1]=g+h
+    i=tuple(i)
+    
+  def aStar(self,start,goal):
+    self.goal=goal
+    print('aStar Search : ')
+    print("open","close",sep='\t\t\t')
+    opened,closed=[],[]
+    opened.append((start,self.gv(start,None)))
+    print(opened, closed, sep='\t\t\t')
+    while opened:
+      p=opened.pop(0)
+      closed.append(p)
+      #Goal node
+      if self.goal == p[0]:
+        print(opened,closed,sep='\t\t\t')
+        print('Goal node found');return
+      #Successors Generation
+      parent=p[0];blank=parent.index(0)
+      # Move Up
+      if blank>=3:
+        v=list(parent)
+        v[blank],v[blank-3]=parent[blank-3],parent[blank]
+        v=tuple(v);g=self.gv(v,parent);h=self.h(v)
+        if (v,g+h) not in opened+closed: opened.append((v,g+h))
+        self.inopen(opened,v,parent,g,h)
+      # Move Down
+      if blank<=5:
+        v=list(parent)
+        v[blank],v[blank+3]=parent[blank+3],parent[blank]
+        v=tuple(v);g=self.gv(v,parent)
+        if (v,g) not in opened+closed: opened.append((v,g))    
+        self.inopen(opened,v,parent,g,h)  
+      # Move Left
+      if blank not in (0,3,6):
+        v=list(parent)
+        v[blank],v[blank-1]=parent[blank-1],parent[blank]
+        v=tuple(v);g=self.gv(v,parent)
+        if (v,g) not in opened+closed: opened.append((v,g))
+        self.inopen(opened,v,parent,g,h)      
+      # Move Right
+      if blank not in (2,5,8):
+        v=list(parent)
+        v[blank],v[blank+1]=parent[blank+1],parent[blank]
+        v=tuple(v);g=self.gv(v,parent)
+        if (v,g) not in opened+closed: opened.append((v,g))
+        self.inopen(opened,v,parent,g,h)  
+      opened.sort(key=lambda x:x[1])
+      print(opened,closed,sep='\t\t\t')
+    print('Goal node not found')
+    
+# Main code :
+start=tuple(int(a) for a in input("Enter start state space seperated: ").split())
+goal=tuple(int(a) for a in input("Enter goal state space seperated: ").split())
+ep=Graph()
+ep.aStar(start,goal)
+
+
+
+
 # Best First :
 class Graph:
   def __init__(self):
